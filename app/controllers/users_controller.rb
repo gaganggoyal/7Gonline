7c0b7cea105871efb484aa77768ff05070d54ae4
gagan goyal <goyalgagan82@gmail.com>
2023-01-27 23:30:07 +0530

    add user show pagewith gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,41 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account info updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to 7G ONLINE #{@user.username.upcase}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
