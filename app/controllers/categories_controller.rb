6ec4b0ee602dbdda414640b18ccc51396bb5015b
gagan goyal <goyalgagan82@gmail.com>
2023-01-28 22:20:58 +0530

    Add Category integration test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+
+  def index
+  
+  end
+
+  def show
+    @category = Category.find(params[:id])
+
+  end
+
+  def new
+    @category = Category.new  
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] =  "Category created successfully"
+      redirect_to @category
+    else
+      render 'new'
+    
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
