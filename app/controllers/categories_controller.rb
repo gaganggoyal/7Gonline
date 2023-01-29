eabe40b4ca0621deec8b69924b71edd5d9a3c7f5
gagan goyal <goyalgagan82@gmail.com>
2023-01-29 19:14:43 +0530

    Add category show edit form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController
-
+  before_action :require_admin, except: [:index, :show]
   def index
-  
+    @categories = Category.paginate(page: params[:page], per_page:5)
   end
 
   def show
     @category = Category.find(params[:id])
-
+    @articles = @category.articles.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
@@ -20,18 +20,34 @@ class CategoriesController < ApplicationController
       redirect_to @category
     else
       render 'new'
-    
     end
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name update successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can use this function"
+      redirect_to categories_path
+    end
   end
 
 end
